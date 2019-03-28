97fd8a9409ddc84552761b8cbfde0767f1274cf9
Rana Mateen <a.mateen858@hotmail.com>
2019-03-28 22:55:11 +0500

    Partial views demo
    partial views demo

---
+++ Partial Views/Controllers/MyController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Partial_Views.Controllers
+{
+    public class MyController : Controller
+    {
+        // GET: 
+        Test test = new Test
+        {
+            SrNo = 1,
+            Name = "Rana Abdul Mateen",
+            Designation = "Full Stack Developer"
+        };
+        List<Test> testList = new List<Test>
+        {
+            new Test{
+            SrNo = 1,
+            Name = "Rana Abdul Mateen",
+            Designation = "Full Stack Developer"
+        },
+
+            new Test{
+            SrNo = 2,
+            Name = "Ali",
+            Designation = "Mobile App Developer"
+        },
+
+            new Test{
+            SrNo = 3,
+            Name = "Ahmed",
+            Designation = "Software Developer"
+        },
+        };
+
+        public ActionResult Index()
+        {
+            ViewBag.abcList = testList;
+            return View();
+        }
+    }
+}
---
+++ Partial Views/Test.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Partial_Views
+{
+    public class Test
+    {
+        public int SrNo { get; set; }
+        public string Name { get; set; }
+        public string Designation { get; set; }
+
+    }
+}
